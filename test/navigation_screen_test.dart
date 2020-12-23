b145b32d4e247d21536f044459af090658f5219f
amitpanda <amitkumar.panda@in.imshealth.com>
2020-12-23 12:15:59 +0530

    testing scaffold created

---
+++ test/navigation_screen_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:electro/main.dart';
+
+void main() {
+  testWidgets('Navigation screen shows default text', (WidgetTester tester) async {
+
+  });
+}
